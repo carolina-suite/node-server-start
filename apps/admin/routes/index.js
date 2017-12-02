61615ca075c60e22d70372ee09f6b1be98fe3455
John F Marion <john.f.marion@gmail.com>
2017-12-02 18:16:51 -0500

    restrict admin to admin users

--- apps/admin/routes/index.js
+++ apps/admin/routes/index.js
@@ -1,6 +1,9 @@
 
 var render = require('../../../site/render');
 
+var authCheck = require('../../auth/middleware/api-auth-check');
+var adminCheck = require('../../auth/middleware/api-admin-check');
+
 function main(request, reply) {
   reply.type('text/html').send(render('SITE/app.pug', {
     appName: 'admin',
@@ -16,13 +19,13 @@ module.exports = function(fastify, options, next) {
 
   fastify.get('/', main);
 
-  fastify.all('/api/list-apps', require('./api/list-apps'));
-  fastify.all('/api/create-object/:model', require('./api/create-object'));
-  fastify.all('/api/list-objects/:model/:page', require('./api/list-objects'));
-  fastify.all('/api/get-object-start/:model', require('./api/get-object-start'));
-  fastify.all('/api/get-object/:model/:object', require('./api/get-object'));
-  fastify.post('/api/update-object/:model/:object', require('./api/update-object'));
-  fastify.post('/api/delete-object/:model/:object', require('./api/delete-object'));
+  fastify.all('/api/list-apps', { beforeHandler: [authCheck, adminCheck] }, require('./api/list-apps'));
+  fastify.all('/api/create-object/:model', { beforeHandler: [authCheck, adminCheck] }, require('./api/create-object'));
+  fastify.all('/api/list-objects/:model/:page', { beforeHandler: [authCheck, adminCheck] }, require('./api/list-objects'));
+  fastify.all('/api/get-object-start/:model', { beforeHandler: [authCheck, adminCheck] }, require('./api/get-object-start'));
+  fastify.all('/api/get-object/:model/:object', { beforeHandler: [authCheck, adminCheck] }, require('./api/get-object'));
+  fastify.post('/api/update-object/:model/:object', { beforeHandler: [authCheck, adminCheck] }, require('./api/update-object'));
+  fastify.post('/api/delete-object/:model/:object', { beforeHandler: [authCheck, adminCheck] }, require('./api/delete-object'));
 
   next();
 };
